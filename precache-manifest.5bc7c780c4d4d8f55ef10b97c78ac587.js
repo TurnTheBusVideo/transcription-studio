4434c802a8604ba779e8fd7f22dc09be9933c5cd
Prasad Thakur <prasad.thakur@sap.com>
2020-07-12 22:28:36 -0700

    Updates

---
+++ precache-manifest.5bc7c780c4d4d8f55ef10b97c78ac587.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9380e04788071da1b98e4ea42eac376d",
+    "url": "/transcription-studio/index.html"
+  },
+  {
+    "revision": "97be6711122720408e62",
+    "url": "/transcription-studio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e89fbb8e7aaac1f379cc",
+    "url": "/transcription-studio/static/css/main.2869d2e5.chunk.css"
+  },
+  {
+    "revision": "97be6711122720408e62",
+    "url": "/transcription-studio/static/js/2.b231a693.chunk.js"
+  },
+  {
+    "revision": "a200174c44a1c583da2ca47c7148e922",
+    "url": "/transcription-studio/static/js/2.b231a693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89fbb8e7aaac1f379cc",
+    "url": "/transcription-studio/static/js/main.2b937875.chunk.js"
+  },
+  {
+    "revision": "3240be39fdf1aae5a95b",
+    "url": "/transcription-studio/static/js/runtime-main.0b480857.js"
+  },
+  {
+    "revision": "47a8ec2c07ee3dcbe63d15e6da20c7cf",
+    "url": "/transcription-studio/static/media/temp.47a8ec2c.pdf"
+  },
+  {
+    "revision": "9824f8fc3d66086563db8c4d2e3fe3c3",
+    "url": "/transcription-studio/static/media/youtube.9824f8fc.png"
+  }
+]);
